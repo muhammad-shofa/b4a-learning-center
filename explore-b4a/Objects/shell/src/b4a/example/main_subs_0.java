0cba0df193360a62fb3d68faf0a7c00f30dfcc69
Muhammad Shofa <mshofa09@gmail.com>
2025-03-27 23:52:27 +0700

    add all files for b4a-explore app proyek

---
+++ explore-b4a/Objects/shell/src/b4a/example/main_subs_0.java
@@ -0,0 +1,116 @@
+package b4a.example;
+
+import anywheresoftware.b4a.BA;
+import anywheresoftware.b4a.pc.*;
+
+public class main_subs_0 {
+
+
+public static RemoteObject  _activity_create(RemoteObject _firsttime) throws Exception{
+try {
+		Debug.PushSubsStack("Activity_Create (main) ","main",0,main.mostCurrent.activityBA,main.mostCurrent,29);
+if (RapidSub.canDelegate("activity_create")) { return b4a.example.main.remoteMe.runUserSub(false, "main","activity_create", _firsttime);}
+Debug.locals.put("FirstTime", _firsttime);
+ BA.debugLineNum = 29;BA.debugLine="Sub Activity_Create(FirstTime As Boolean)";
+Debug.ShouldStop(268435456);
+ BA.debugLineNum = 30;BA.debugLine="Activity.LoadLayout(\"Layout\")";
+Debug.ShouldStop(536870912);
+main.mostCurrent._activity.runMethodAndSync(false,"LoadLayout",(Object)(RemoteObject.createImmutable("Layout")),main.mostCurrent.activityBA);
+ BA.debugLineNum = 31;BA.debugLine="End Sub";
+Debug.ShouldStop(1073741824);
+return RemoteObject.createImmutable("");
+}
+catch (Exception e) {
+			throw Debug.ErrorCaught(e);
+		} 
+finally {
+			Debug.PopSubsStack();
+		}}
+public static RemoteObject  _activity_pause(RemoteObject _userclosed) throws Exception{
+try {
+		Debug.PushSubsStack("Activity_Pause (main) ","main",0,main.mostCurrent.activityBA,main.mostCurrent,37);
+if (RapidSub.canDelegate("activity_pause")) { return b4a.example.main.remoteMe.runUserSub(false, "main","activity_pause", _userclosed);}
+Debug.locals.put("UserClosed", _userclosed);
+ BA.debugLineNum = 37;BA.debugLine="Sub Activity_Pause (UserClosed As Boolean)";
+Debug.ShouldStop(16);
+ BA.debugLineNum = 39;BA.debugLine="End Sub";
+Debug.ShouldStop(64);
+return RemoteObject.createImmutable("");
+}
+catch (Exception e) {
+			throw Debug.ErrorCaught(e);
+		} 
+finally {
+			Debug.PopSubsStack();
+		}}
+public static RemoteObject  _activity_resume() throws Exception{
+try {
+		Debug.PushSubsStack("Activity_Resume (main) ","main",0,main.mostCurrent.activityBA,main.mostCurrent,33);
+if (RapidSub.canDelegate("activity_resume")) { return b4a.example.main.remoteMe.runUserSub(false, "main","activity_resume");}
+ BA.debugLineNum = 33;BA.debugLine="Sub Activity_Resume";
+Debug.ShouldStop(1);
+ BA.debugLineNum = 35;BA.debugLine="End Sub";
+Debug.ShouldStop(4);
+return RemoteObject.createImmutable("");
+}
+catch (Exception e) {
+			throw Debug.ErrorCaught(e);
+		} 
+finally {
+			Debug.PopSubsStack();
+		}}
+public static RemoteObject  _button1_click() throws Exception{
+try {
+		Debug.PushSubsStack("Button1_Click (main) ","main",0,main.mostCurrent.activityBA,main.mostCurrent,41);
+if (RapidSub.canDelegate("button1_click")) { return b4a.example.main.remoteMe.runUserSub(false, "main","button1_click");}
+ BA.debugLineNum = 41;BA.debugLine="Sub Button1_Click";
+Debug.ShouldStop(256);
+ BA.debugLineNum = 42;BA.debugLine="xui.MsgboxAsync(\"Coba Merubah Message Box\", \"B4X\"";
+Debug.ShouldStop(512);
+main._xui.runVoidMethod ("MsgboxAsync",main.processBA,(Object)(BA.ObjectToCharSequence("Coba Merubah Message Box")),(Object)(BA.ObjectToCharSequence(RemoteObject.createImmutable("B4X"))));
+ BA.debugLineNum = 43;BA.debugLine="End Sub";
+Debug.ShouldStop(1024);
+return RemoteObject.createImmutable("");
+}
+catch (Exception e) {
+			throw Debug.ErrorCaught(e);
+		} 
+finally {
+			Debug.PopSubsStack();
+		}}
+public static RemoteObject  _globals() throws Exception{
+ //BA.debugLineNum = 21;BA.debugLine="Sub Globals";
+ //BA.debugLineNum = 23;BA.debugLine="Private Button1 As Button";
+main.mostCurrent._button1 = RemoteObject.createNew ("anywheresoftware.b4a.objects.ButtonWrapper");
+ //BA.debugLineNum = 24;BA.debugLine="Private LblHeader1 As Label";
+main.mostCurrent._lblheader1 = RemoteObject.createNew ("anywheresoftware.b4a.objects.LabelWrapper");
+ //BA.debugLineNum = 25;BA.debugLine="Private LblDate1 As Label";
+main.mostCurrent._lbldate1 = RemoteObject.createNew ("anywheresoftware.b4a.objects.LabelWrapper");
+ //BA.debugLineNum = 26;BA.debugLine="Private CheckBox1 As CheckBox";
+main.mostCurrent._checkbox1 = RemoteObject.createNew ("anywheresoftware.b4a.objects.CompoundButtonWrapper.CheckBoxWrapper");
+ //BA.debugLineNum = 27;BA.debugLine="End Sub";
+return RemoteObject.createImmutable("");
+}
+
+public static void initializeProcessGlobals() {
+    
+    if (main.processGlobalsRun == false) {
+	    main.processGlobalsRun = true;
+		try {
+		        main_subs_0._process_globals();
+starter_subs_0._process_globals();
+main.myClass = BA.getDeviceClass ("b4a.example.main");
+starter.myClass = BA.getDeviceClass ("b4a.example.starter");
+		
+        } catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+    }
+}public static RemoteObject  _process_globals() throws Exception{
+ //BA.debugLineNum = 15;BA.debugLine="Sub Process_Globals";
+ //BA.debugLineNum = 18;BA.debugLine="Private xui As XUI";
+main._xui = RemoteObject.createNew ("anywheresoftware.b4a.objects.B4XViewWrapper.XUI");
+ //BA.debugLineNum = 19;BA.debugLine="End Sub";
+return RemoteObject.createImmutable("");
+}
+}
